d38cb7680373c013148243cf38d3becc13f6e301
gitbash <kolhenagesh2017@gmail.com>
2021-05-12 22:31:07 +0530

    Added:Calculate Daily Employee Wage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,14 +1,18 @@
-
 public class EmpWageBuilder {
-
 	public static void main(String[] args) {
 		//constants
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HR = 20;
+		//variables
+		int empHrs=0;
+		int empWage=0;
 		//computation
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == IS_FULL_TIME)
-			System.out.println("Employee is Present");
+		    empHrs=8;
 		else
-			System.out.println("Employee is Abscent");	
+			empHrs=0;
+		empWage = empHrs * EMP_RATE_PER_HR;
+		System.out.println("Emp Wage: " + empWage);
 	}
 }
