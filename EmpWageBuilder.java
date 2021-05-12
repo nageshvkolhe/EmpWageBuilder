93adb45be2b2675264c0392e4f91b04a8a8f2646
gitbash <kolhenagesh2017@gmail.com>
2021-05-12 23:05:10 +0530

    Added: Wages for Month

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,14 +1,16 @@
 public class EmpWageBuilder {
-	    //constants
-	    public static final int IS_PART_TIME = 1;
-	    public static final int IS_FULL_TIME = 2;
-	    public static final int EMP_RATE_PER_HR = 20;
-
-public static void main(String[] args) {
+	     public static final int IS_PART_TIME = 1;
+	     public static final int IS_FULL_TIME = 2;
+	     public static final int EMP_RATE_PER_HR = 20;
+	     public static final int NUM_OF_WORKING_DAYS = 20;
+	     
+   public static void main(String[] args) {
 		//variables
 		int empHrs=0;
 		int empWage=0;
+		int totalEmpWage=0;
 		//computation
+		for (int day =0; day < NUM_OF_WORKING_DAYS; day++) {
 		int empCheck =(int) Math.floor(Math.random() * 10) % 3;
 		switch (empCheck) {
 		    case IS_PART_TIME:
@@ -21,6 +23,9 @@ public static void main(String[] args) {
 			  empHrs=0;
 		}
 			empWage = empHrs * EMP_RATE_PER_HR;
+			totalEmpWage += empWage;
 			System.out.println("Emp Wage: " + empWage);
-     } 	
+   } 	
+		System.out.println("Total Emp Wage: " + totalEmpWage);
+  }
 }
