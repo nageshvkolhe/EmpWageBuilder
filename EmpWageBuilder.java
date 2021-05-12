24dd5a0d792bad3b8eca4420ce492e1685ece798
gitbash <kolhenagesh2017@gmail.com>
2021-05-12 22:54:15 +0530

    Added: Part time Employee & Wage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,15 +1,18 @@
 public class EmpWageBuilder {
 	public static void main(String[] args) {
 		//constants
-		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 1;
+		int IS_FULL_TIME = 2;
 		int EMP_RATE_PER_HR = 20;
 		//variables
 		int empHrs=0;
 		int empWage=0;
 		//computation
 		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == IS_FULL_TIME)
-		    empHrs=8;
+		if (empCheck == IS_PART_TIME)
+		    empHrs=4;
+		else if (empCheck == IS_PART_TIME)
+			empHrs=4;
 		else
 			empHrs=0;
 		empWage = empHrs * EMP_RATE_PER_HR;
